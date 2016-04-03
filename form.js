84036556b6764b31dbdba29c20d837ca642b067a
Andrew Piper <threax@users.noreply.github.com>
2016-04-03 17:36:55 -0400

    Split up htmlrest.

---
+++ form.js
@@ -0,0 +1,34 @@
+﻿//Form Functions
+//htmlrest.classes.form = function () {
+//    
+//};
+
+htmlrest.form = new function () {
+    function submitRunner(form, evt, sender, previousResult, runner) {
+        $.ajax({
+            method: form.attr('method'),
+            url: form.attr('action'),
+            data: form.serialize(),
+            success: function (data, textStatus, jqXHR) {
+                runner.next(jqXHR);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                runner.next(jqXHR);
+            }
+        });
+    }
+
+    this.submit = function (form) {
+        return function (evt, sender, previousResult, runner) {
+            submitRunner(form, evt, sender, previousResult, runner);
+        }
+    }
+
+    this.submitSelf = function () {
+        return function (evt, sender, previousResult, runner) {
+            submitRunner(sender, evt, sender, previousResult, runner);
+        }
+    }
+}
+
+//htmlrest.form = new htmlrest.classes.form();
---
+++ output.js
@@ -0,0 +1,32 @@
+﻿//Output Function
+htmlrest.output = new function () {
+    function httpResultRunner(element, evt, sender, previousResult, runner) {
+        element.html('did something');
+
+        var errorClass = element.attr('data-class-error');
+        if (errorClass) {
+            if (success) {
+                element.removeClass(errorClass);
+            }
+            else {
+                element.addClass(errorClass);
+            }
+        }
+
+        var successClass = element.attr('data-class-success');
+        if (successClass) {
+            if (success) {
+                element.addClass(successClass);
+            }
+            else {
+                element.removeClass(successClass);
+            }
+        }
+    };
+
+    this.httpResult = function (element) {
+        return function (evt, sender, previousResult, runner) {
+            httpResultRunner(element, evt, sender, previousResult, runner);
+        };
+    }
+};
