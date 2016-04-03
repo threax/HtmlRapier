4f479487dcf861e4e816f2b2e090a0488507b8aa
Andrew Piper <threax@users.noreply.github.com>
2016-04-03 16:53:04 -0400

    Added ouput.

--- htmlrest.js
+++ htmlrest.js
@@ -28,35 +28,39 @@ htmlrest.er = function (returnVal, functions)
 }
 
 //Output Function
-//htmlrest.classes.output = function() {
-//    
-//};
-//
-//htmlrest.classes.output.prototype.httpResult = function (target, evt, sender, previousResult, runner) {
-//    target.html('did something');
-//
-//    var errorClass = target.attr('data-class-error');
-//    if (errorClass) {
-//        if (success) {
-//            target.removeClass(errorClass);
-//        }
-//        else {
-//            target.addClass(errorClass);
-//        }
-//    }
-//
-//    var successClass = target.attr('data-class-success');
-//    if (successClass) {
-//        if (success) {
-//            target.addClass(successClass);
-//        }
-//        else {
-//            target.removeClass(successClass);
-//        }
-//    }
-//};
-//
-//htmlrest.output = new htmlrest.classes.output();
+htmlrest.classes.output = function() {
+    function httpResultRunner(element, evt, sender, previousResult, runner) {
+        element.html('did something');
+
+        var errorClass = element.attr('data-class-error');
+        if (errorClass) {
+            if (success) {
+                element.removeClass(errorClass);
+            }
+            else {
+                element.addClass(errorClass);
+            }
+        }
+
+        var successClass = element.attr('data-class-success');
+        if (successClass) {
+            if (success) {
+                element.addClass(successClass);
+            }
+            else {
+                element.removeClass(successClass);
+            }
+        }
+    };
+
+    this.httpResult = function (element) {
+        return function (evt, sender, previousResult, runner) {
+            httpResultRunner(element, evt, sender, previousResult, runner);
+        };
+    }
+};
+
+htmlrest.output = new htmlrest.classes.output();
 
 //Form Functions
 htmlrest.classes.form = function () {
@@ -86,24 +90,5 @@ htmlrest.classes.form = function () {
         }
     }
 };
-
-//htmlrest.runners.form.prototype.submit = function (form, evt, sender, previousResult, runner) {
-//    $.ajax({
-//        method: form.attr('method'),
-//        url: form.attr('action'),
-//        data: form.serialize(),
-//        success: function (data, textStatus, jqXHR) {
-//            runner.next(jqXHR);
-//        },
-//        error: function (jqXHR, textStatus, errorThrown) {
-//            runner.next(jqXHR);
-//        }
-//    });
-//};
-
-//htmlrest.classes.form.prototype.submitHandler = function (evt) {
-//    htmlrest.form.submit($(this));
-//    return false;
-//}
 
 htmlrest.form = new htmlrest.classes.form();
