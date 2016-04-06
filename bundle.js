1fbed56ed4ebea0a984eaa754285ac2c8a6a5c6f
Andrew Piper <threax@users.noreply.github.com>
2016-04-05 23:02:12 -0400

    Got rid of extra functions.

--- bundle.js
+++ bundle.js
@@ -53,42 +53,14 @@ htmlrest.event.prototype.rest = function () { }
 
 htmlrest.event.prototype.rest.prototype.post = function (url) {
     return function (evt, sender, previousResult, runner) {
-        htmlrest.event.prototype.rest.prototype.post.prototype.runner(url, evt, sender, previousResult, runner);
+        htmlrest.event.prototype.rest.prototype.ajax.prototype.runner(url, 'post', evt, sender, previousResult, runner);
     }
-}
-
-htmlrest.event.prototype.rest.prototype.post.prototype.runner = function (url, evt, sender, previousResult, runner) {
-    $.ajax({
-        method: 'post',
-        url: previousResult,
-        data: data,
-        success: function (retData, textStatus, jqXHR) {
-            runner.next({ data: retData, jqXHR: jqXHR, success: true });
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            runner.next({ data: jqXHR.data, jqXHR: jqXHR, success: false });
-        }
-    });
 }
 
 htmlrest.event.prototype.rest.prototype.put = function (url) {
     return function (evt, sender, previousResult, runner) {
-        htmlrest.event.prototype.rest.prototype.put.prototype.runner(url, evt, sender, previousResult, runner);
+        htmlrest.event.prototype.rest.prototype.ajax.prototype.runner(url, 'put', evt, sender, previousResult, runner);
     }
-}
-
-htmlrest.event.prototype.rest.prototype.put.prototype.runner = function (url, evt, sender, previousResult, runner) {
-    $.ajax({
-        method: 'put',
-        url: url,
-        data: previousResult,
-        success: function (retData, textStatus, jqXHR) {
-            runner.next({ data: retData, jqXHR: jqXHR, success: true });
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            runner.next({ data: jqXHR.data, jqXHR: jqXHR, success: false });
-        }
-    });
 }
 
 htmlrest.event.prototype.rest.prototype.get = function (url) {
