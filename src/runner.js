c153936b7d0ea06facbc9344e26bd123572c0cc4
Andrew Piper <threax@users.noreply.github.com>
2016-06-04 13:10:01 -0400

    Changed when functions create their arrays, now create a function that returns an array of actions to run, maakes definitions really verbose, however.

--- src/runner.js
+++ src/runner.js
@@ -1,22 +1,28 @@
 ﻿var htmlrest = htmlrest || {};
 
-htmlrest.event = function (functions, returnVal) {
-        if (returnVal === undefined) {
-            returnVal = false;
-        }
+htmlrest.event = function (functionCreator, returnVal)
+{
+    if (returnVal === undefined)
+    {
+        returnVal = false;
+    }
 
-        return new htmlrest.event.prototype.runner(functions, returnVal, null);
+    return new htmlrest.event.prototype.runner(functionCreator, returnVal, null);
 }
 
-htmlrest.run = function (source, functions) {
-    var runner = new htmlrest.event.prototype.runner(functions, false, source);
+htmlrest.run = function (source, functionCreator)
+{
+    var runner = new htmlrest.event.prototype.runner(functionCreator, false, source);
     runner();
 }
 
-htmlrest.func = function (func) {
-    return function (evt, sender, previousResult, runner) {
+htmlrest.func = function (func)
+{
+    return function (evt, sender, previousResult, runner)
+    {
         var result = func(previousResult);
-        if (!result) {
+        if (!result)
+        {
             result = previousResult;
         }
         runner.next(result);
@@ -24,23 +30,30 @@ htmlrest.func = function (func) {
 }
 
 //Defining classes on event's prototype
-htmlrest.event.prototype.runner = function (functions, returnVal, sender) {
+htmlrest.event.prototype.runner = function (functionCreator, returnVal, sender)
+{
     var self = this;
-    var functions = functions;
+    var functionCreator = functionCreator;
     var returnVal = returnVal;
+    var functions = null;
     var currentFunc = -1;
     var sender = sender;
     var event = null;
 
-    this.next = function (previousResult) {
-        if (++currentFunc < functions.length) {
+    this.next = function (previousResult)
+    {
+        if (++currentFunc < functions.length)
+        {
             functions[currentFunc](event, sender, previousResult, self);
         }
     }
 
-    return function (evt) {
+    return function (evt)
+    {
+        functions = functionCreator();
         currentFunc = -1;
-        if (!sender) {
+        if (!sender)
+        {
             sender = $(this);
         }
         event = evt;
