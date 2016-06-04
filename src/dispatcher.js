35af50b0cae3b58d24de39d354924315d5e8107b
Andrew Piper <threax@users.noreply.github.com>
2016-06-04 15:29:18 -0400

    Added a simple dispatcher.

---
+++ src/dispatcher.js
@@ -0,0 +1,34 @@
+﻿htmlrest.dispatch = function (name, data)
+{
+    if (htmlrest.dispatch.prototype.events.hasOwnProperty(name))
+    {
+        htmlrest.dispatch.prototype.events[name].forEach(function (element, index, array)
+        {
+            element(data);
+        });
+    }
+}
+
+htmlrest.dispatch.prototype.events = {};
+
+htmlrest.bind = function (name, callback)
+{
+    if (!htmlrest.dispatch.prototype.events.hasOwnProperty(name))
+    {
+        htmlrest.dispatch.prototype.events[name] = [];
+    }
+    htmlrest.dispatch.prototype.events[name].push(callback);
+}
+
+htmlrest.unbind = function (name, callback)
+{
+    if (htmlrest.dispatch.prototype.events.hasOwnProperty(name))
+    {
+        var array = htmlrest.dispatch.prototype.events[name];
+        var index = array.indexOf(callback);
+        if (index > -1)
+        {
+            array.splice(index, 1);
+        }
+    }
+}
