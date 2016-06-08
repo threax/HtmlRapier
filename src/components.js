ac3cdd18ed9df8b0249010159a58199c8db97dd1
Andrew Piper <piper.andrew@spcollege.edu>
2016-06-08 13:47:14 -0400

    Added a way to register and construct components.

---
+++ src/components.js
@@ -0,0 +1,18 @@
+﻿htmlrest.createComponent = function (name, data, parentComponent)
+{
+    if (typeof(parentComponent) === 'string'){
+        parentComponent = $(parentComponent);
+    }
+    if (parentComponent instanceof jQuery) {
+        parentComponent = parentComponent[0];
+    }
+    if (htmlrest.createComponent.prototype.factory.hasOwnProperty(name)) {
+        return htmlrest.createComponent.prototype.factory[name](data, parentComponent);
+    }
+}
+
+htmlrest.registerComponent = function (name, createFunc) {
+    htmlrest.createComponent.prototype.factory[name] = createFunc;
+}
+
+htmlrest.createComponent.prototype.factory = {};
