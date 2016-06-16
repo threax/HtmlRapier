969cf580096bba19dc61a703e971836c49af9cb5
Andrew Piper <piper.andrew@spcollege.edu>
2016-06-16 12:50:32 -0400

    Added a way to recover a json object directly from session storage.

--- src/storage.js
+++ src/storage.js
@@ -1,4 +1,18 @@
 ﻿htmlrest.storage =
 {
-    startupData: {}
+    startupData: {},
+    getSessionStorageJson: function(name, defaultValue){
+        if (defaultValue === undefined) {
+            defaultValue = {};
+        }
+
+        var recovered = sessionStorage.getItem(name);
+        if (recovered !== null) {
+            recovered = JSON.parse(recovered);
+        }
+        else {
+            recovered = defaultValue;
+        }
+        return recovered;
+    }
 };
