e85d879c83cc7b288c363127b07fc1108bd18061
Andrew Piper <piper.andrew@spcollege.edu>
2016-07-07 12:54:25 -0400

    Added lifecycle file.

---
+++ src/lifecycle.js
@@ -0,0 +1,52 @@
+﻿htmlrest.lifecycle = htmlrest.lifecycle || {
+    ajaxLoad: function (settings) {
+        var mainDisplay = undefined;
+        if (settings.mainDisplayQuery) {
+            mainDisplay = $(settings.mainDisplayQuery);
+        }
+
+        var loadingFailDisplay = undefined;
+        if (settings.loadingFailDisplayQuery) {
+            loadingFailDisplay = $(settings.loadingFailDisplayQuery);
+        }
+
+        var loadingDisplay = undefined;
+        if (settings.loadingDisplayQuery) {
+            loadingDisplay = $(settings.loadingDisplayQuery);
+        }
+
+        //Display Functions
+        this.loading = function() {
+            hideAll();
+            if (loadingDisplay) {
+                loadingDisplay.show();
+            }
+        }
+
+        this.failed = function () {
+            hideAll();
+            if (loadingFailDisplay) {
+                loadingFailDisplay.show();
+            }
+        }
+
+        this.succeeded = function () {
+            hideAll();
+            if (mainDisplay) {
+                mainDisplay.show();
+            }
+        }
+
+        function hideAll() {
+            if (mainDisplay) {
+                mainDisplay.hide();
+            }
+            if (loadingFailDisplay) {
+                loadingFailDisplay.hide();
+            }
+            if (loadingDisplay) {
+                loadingDisplay.hide();
+            }
+        }
+    }
+}
