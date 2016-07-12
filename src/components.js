7f07a4eb722e1be8a326fe3508d35f97d8317193
Andrew Piper <piper.andrew@spcollege.edu>
2016-07-12 15:15:58 -0400

    More jquery elimination, added a way to write dom elements. Also made html templates work like component items instead of the suggested template way, this way we can still do our format strings to these templates.

--- src/components.js
+++ src/components.js
@@ -1,7 +1,6 @@
 ﻿"use strict";
 
 (function () {
-    var supportsTemplates = 'content' in document.createElement('template');
 
     htmlrest.createComponent = function (name, data, parentComponent, createdCallback) {
         parentComponent = htmlrest.component.getPlainElement(parentComponent);
@@ -11,20 +10,6 @@
                 createdCallback(created, data);
             }
             return created;
-        }
-
-        //Also support template based creation if available
-        if (supportsTemplates) {
-            var template = document.querySelector('#' + name);
-            if (template != null && template.tagName === "TEMPLATE") {
-                created = document.importNode(template.content, true);
-                parentComponent.appendChild(created);
-
-                if (createdCallback !== undefined) {
-                    createdCallback(created, data);
-                }
-                return created;
-            }
         }
     }
 
@@ -64,7 +49,7 @@
         //Get Plain Javascript Element
         getPlainElement: function (element) {
             if (typeof (element) === 'string') {
-                element = $(element);
+                element = s(element)[0];
             }
             if (element instanceof jQuery) {
                 element = element[0];
@@ -74,19 +59,19 @@
     };
 
     //Auto find components on the page and build them as components
-    (function ($, s, h) {
+    (function (s, h) {
         //Component creation function
         function createItem(data, componentString, parentComponent) {
             var itemMarkup = h.formatText(componentString, data);
-            var appendItem = $(parentComponent);
-            var newItem = $(itemMarkup);
-            newItem.appendTo(appendItem);
+            var newItem = str2DOMElement(itemMarkup);
+            parentComponent.appendChild(newItem);
 
-            return newItem[0];
+            return newItem;
         }
 
         var attrName = "data-htmlrest-component";
         var componentElements = s('[' + attrName + ']');
+        document.get
         //Read components backward, removing children from parents along the way.
         for (var i = componentElements.length - 1; i >= 0; --i) {
             (function () {
@@ -102,22 +87,69 @@
             })();
         };
 
-        //If we don't support templates, extract them as components also
-        if (!supportsTemplates) {
-            var templateElements = document.getElementsByTagName("TEMPLATE");
-            while(templateElements.length > 0) {
-                (function () {
-                    var element = templateElements[0];
-                    var componentName = element.getAttribute("id");
-                    var componentString = element.innerHTML;
-                    element.parentNode.removeChild(element);
+        //Also grab the templates from the page and use them too
+        var templateElements = document.getElementsByTagName("TEMPLATE");
+        while(templateElements.length > 0) {
+            (function () {
+                var element = templateElements[0];
+                var componentName = element.getAttribute("id");
+                var componentString = element.innerHTML.trim();
+                element.parentNode.removeChild(element);
 
-                    h.registerComponent(componentName, function (data, parentComponent) {
-                        return createItem(data, componentString, parentComponent);
-                    });
-                })();
-            }
+                h.registerComponent(componentName, function (data, parentComponent) {
+                    return createItem(data, componentString, parentComponent);
+                });
+            })();
         }
 
-    })(jQuery, Sizzle, htmlrest);
+    })(Sizzle, htmlrest);
+
+    //From jQuery and the discussion on http://krasimirtsonev.com/blog/article/Revealing-the-magic-how-to-properly-convert-HTML-string-to-a-DOM-element
+    var str2DOMElement = function (html) {
+        var wrapMap = {
+            option: [1, "<select multiple='multiple'>", "</select>"],
+            legend: [1, "<fieldset>", "</fieldset>"],
+            area: [1, "<map>", "</map>"],
+            param: [1, "<object>", "</object>"],
+            thead: [1, "<table>", "</table>"],
+            tr: [2, "<table><tbody>", "</tbody></table>"],
+            col: [2, "<table><tbody></tbody><colgroup>", "</colgroup></table>"],
+            td: [3, "<table><tbody><tr>", "</tr></tbody></table>"],
+            body: [0, "", ""],
+            _default: [1, "<div>", "</div>"]
+        };
+        wrapMap.optgroup = wrapMap.option;
+        wrapMap.tbody = wrapMap.tfoot = wrapMap.colgroup = wrapMap.caption = wrapMap.thead;
+        wrapMap.th = wrapMap.td;
+        var match = /<\s*\w.*?>/g.exec(html);
+        var element = document.createElement('div');
+        if (match != null) {
+            var tag = match[0].replace(/</g, '').replace(/>/g, '').split(' ')[0];
+            if (tag.toLowerCase() === 'body') {
+                var dom = document.implementation.createDocument('http://www.w3.org/1999/xhtml', 'html', null);
+                var body = document.createElement("body");
+                // keeping the attributes
+                element.innerHTML = html.replace(/<body/g, '<div').replace(/<\/body>/g, '</div>');
+                var attrs = element.firstChild.attributes;
+                body.innerHTML = html;
+                for (var i = 0; i < attrs.length; i++) {
+                    body.setAttribute(attrs[i].name, attrs[i].value);
+                }
+                return body;
+            } else {
+                var map = wrapMap[tag] || wrapMap._default, element;
+                html = map[1] + html + map[2];
+                element.innerHTML = html;
+                // Descend through wrappers to the right content
+                var j = map[0] + 1;
+                while (j--) {
+                    element = element.lastChild;
+                }
+            }
+        } else {
+            element.innerHTML = html;
+            element = element.lastChild;
+        }
+        return element;
+    }
 })();
--- src/form.js
+++ src/form.js
@@ -1,158 +1,163 @@
-﻿//Form Functions
-htmlrest.form = htmlrest.form || {
-    serialize: function (form) {
-        //This is from https://code.google.com/archive/p/form-serialize/downloads
-        //Modified to return an object instead of a query string
-        form = htmlrest.component.getPlainElement(form);
+﻿(function (Sizzle) {
+    //Form Functions
+    htmlrest.form = htmlrest.form || {
+        serialize: function (form) {
+            //This is from https://code.google.com/archive/p/form-serialize/downloads
+            //Modified to return an object instead of a query string
+            form = htmlrest.component.getPlainElement(form);
 
-        if (!form || form.nodeName !== "FORM") {
-            return;
-        }
-        var i, j, q = {};
-        for (i = form.elements.length - 1; i >= 0; i = i - 1) {
-            if (form.elements[i].name === "") {
-                continue;
+            if (!form || form.nodeName !== "FORM") {
+                return;
             }
-            switch (form.elements[i].nodeName) {
-                case 'INPUT':
-                    switch (form.elements[i].type) {
-                        case 'text':
-                        case 'hidden':
-                        case 'password':
-                        case 'button':
-                        case 'reset':
-                        case 'submit':
-                            q[form.elements[i].name] = form.elements[i].value;
-                            break;
-                        case 'checkbox':
-                        case 'radio':
-                            if (form.elements[i].checked) {
+            var i, j, q = {};
+            for (i = form.elements.length - 1; i >= 0; i = i - 1) {
+                if (form.elements[i].name === "") {
+                    continue;
+                }
+                switch (form.elements[i].nodeName) {
+                    case 'INPUT':
+                        switch (form.elements[i].type) {
+                            case 'text':
+                            case 'hidden':
+                            case 'password':
+                            case 'button':
+                            case 'reset':
+                            case 'submit':
                                 q[form.elements[i].name] = form.elements[i].value;
-                            }
-                            break;
-                        case 'file':
-                            break;
-                    }
-                    break;
-                case 'TEXTAREA':
-                    q[form.elements[i].name] = form.elements[i].value;
-                    break;
-                case 'SELECT':
-                    switch (form.elements[i].type) {
-                        case 'select-one':
-                            q[form.elements[i].name] = form.elements[i].value;
-                            break;
-                        case 'select-multiple':
-                            for (j = form.elements[i].options.length - 1; j >= 0; j = j - 1) {
-                                if (form.elements[i].options[j].selected) {
-                                    q[form.elements[i].name] = form.elements[i].options[j].value;
+                                break;
+                            case 'checkbox':
+                            case 'radio':
+                                if (form.elements[i].checked) {
+                                    q[form.elements[i].name] = form.elements[i].value;
                                 }
-                            }
-                            break;
-                    }
-                    break;
-                case 'BUTTON':
-                    switch (form.elements[i].type) {
-                        case 'reset':
-                        case 'submit':
-                        case 'button':
-                            q[form.elements[i].name] = form.elements[i].value;
-                            break;
-                    }
-                    break;
+                                break;
+                            case 'file':
+                                break;
+                        }
+                        break;
+                    case 'TEXTAREA':
+                        q[form.elements[i].name] = form.elements[i].value;
+                        break;
+                    case 'SELECT':
+                        switch (form.elements[i].type) {
+                            case 'select-one':
+                                q[form.elements[i].name] = form.elements[i].value;
+                                break;
+                            case 'select-multiple':
+                                for (j = form.elements[i].options.length - 1; j >= 0; j = j - 1) {
+                                    if (form.elements[i].options[j].selected) {
+                                        q[form.elements[i].name] = form.elements[i].options[j].value;
+                                    }
+                                }
+                                break;
+                        }
+                        break;
+                    case 'BUTTON':
+                        switch (form.elements[i].type) {
+                            case 'reset':
+                            case 'submit':
+                            case 'button':
+                                q[form.elements[i].name] = form.elements[i].value;
+                                break;
+                        }
+                        break;
+                }
             }
-        }
-        return q;
-    },
+            return q;
+        },
 
-    populate: function (form, data) {
-        form.find('[name]').each(function () {
-            $(this).val(data[$(this).attr('name')]);
-        });
-    },
-
-    ajaxLifecycle: function (settings) {
-        var form = $(settings.formQuery); //form is required
-
-        var mainDisplay = undefined;
-        if (settings.mainDisplayQuery) {
-            mainDisplay = $(settings.mainDisplayQuery);
-        }
-
-        var populateFailDisplay = undefined;
-        if (settings.populateFailDisplayQuery) {
-            populateFailDisplay = $(settings.populateFailDisplayQuery);
-        }
-
-        var loadingDisplay = undefined;
-        if (settings.loadingDisplayQuery) {
-            loadingDisplay = $(settings.loadingDisplayQuery);
-        }
-
-        //Populate
-        this.populateData = function () {
-            loading();
-            var url = form.attr('action');
-            htmlrest.rest.get(url, getSuccess, getFail);
-        }
-
-        function getSuccess(data) {
-            hideAll();
-            htmlrest.form.populate(form, data);
-            if (mainDisplay) {
-                mainDisplay.show();
+        populate: function (form, data) {
+            form = htmlrest.component.getPlainElement(form);
+            var nameAttrs = Sizzle('[name]', form);
+            for (var i = 0; i < nameAttrs.length; ++i) {
+                var element = nameAttrs[i];
+                element.value = data[element.getAttribute('name')];
             }
-        }
+        },
 
-        function getFail(data) {
-            hideAll();
-            if (populateFailDisplay) {
-                populateFailDisplay.show();
-            }
-        }
+        ajaxLifecycle: function (settings) {
+            var form = $(settings.formQuery); //form is required
 
-        //Submit
-        form.submit(function () {
-            loading();
-            var url = form.attr('action');
-            var data = htmlrest.form.serialize(form);
-            htmlrest.rest.post(url, data, postSuccess, postFail);
-            return false;
-        });
+            var mainDisplay = undefined;
+            if (settings.mainDisplayQuery) {
+                mainDisplay = $(settings.mainDisplayQuery);
+            }
 
-        function postSuccess(data) {
-            hideAll();
-            if (mainDisplay) {
-                mainDisplay.show();
+            var populateFailDisplay = undefined;
+            if (settings.populateFailDisplayQuery) {
+                populateFailDisplay = $(settings.populateFailDisplayQuery);
             }
-        }
 
-        function postFail(data) {
-            hideAll();
-            if (mainDisplay) {
-                mainDisplay.show();
+            var loadingDisplay = undefined;
+            if (settings.loadingDisplayQuery) {
+                loadingDisplay = $(settings.loadingDisplayQuery);
             }
-            alert(data.message); //temp
-        }
 
-        //Display Functions
-        function loading() {
-            hideAll();
-            if (loadingDisplay) {
-                loadingDisplay.show();
+            //Populate
+            this.populateData = function () {
+                loading();
+                var url = form.attr('action');
+                htmlrest.rest.get(url, getSuccess, getFail);
             }
-        }
 
-        function hideAll() {
-            if (mainDisplay) {
-                mainDisplay.hide();
+            function getSuccess(data) {
+                hideAll();
+                htmlrest.form.populate(form, data);
+                if (mainDisplay) {
+                    mainDisplay.show();
+                }
             }
-            if (populateFailDisplay) {
-                populateFailDisplay.hide();
+
+            function getFail(data) {
+                hideAll();
+                if (populateFailDisplay) {
+                    populateFailDisplay.show();
+                }
             }
-            if (loadingDisplay) {
-                loadingDisplay.hide();
+
+            //Submit
+            form.submit(function () {
+                loading();
+                var url = form.attr('action');
+                var data = htmlrest.form.serialize(form);
+                htmlrest.rest.post(url, data, postSuccess, postFail);
+                return false;
+            });
+
+            function postSuccess(data) {
+                hideAll();
+                if (mainDisplay) {
+                    mainDisplay.show();
+                }
+            }
+
+            function postFail(data) {
+                hideAll();
+                if (mainDisplay) {
+                    mainDisplay.show();
+                }
+                alert(data.message); //temp
+            }
+
+            //Display Functions
+            function loading() {
+                hideAll();
+                if (loadingDisplay) {
+                    loadingDisplay.show();
+                }
+            }
+
+            function hideAll() {
+                if (mainDisplay) {
+                    mainDisplay.hide();
+                }
+                if (populateFailDisplay) {
+                    populateFailDisplay.hide();
+                }
+                if (loadingDisplay) {
+                    loadingDisplay.hide();
+                }
             }
         }
     }
-}
+})(Sizzle);
