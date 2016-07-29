f32258cb5be4b292e0628ee2025dbeaed58b39f9
Andrew Piper <piper.andrew@spcollege.edu>
2016-07-29 11:28:52 -0400

    Added a navmenu class that helps track client side constructed menus.

--- htmlrest.js
+++ htmlrest.js
@@ -1850,6 +1850,46 @@ function(exports, module, toggles){
         return toggle;
     });
 });
+jsns.define("htmlrest.widgets.navmenu", [
+    "htmlrest.eventhandler",
+    "htmlrest.controller"
+],
+function (exports, module, EventHandler, controller) {
+    var navMenus = {};
+
+    function NavMenu() {
+        var menuItems = [];
+
+        var itemAdded = new EventHandler();
+        this.itemAdded = itemAdded.modifier;
+
+        function add(name, controllerConstructor) {
+            if (controllerConstructor !== undefined) {
+                controllerConstructor = controller.createOnCallback(controllerConstructor);
+            }
+            menuItems.push({
+                name: name,
+                created: controllerConstructor
+            });
+            itemAdded.fire(item);
+        }
+        this.add = add;
+
+        function getItems() {
+            return menuItems;
+        }
+        this.getItems = getItems;
+    }
+
+    function getNavMenu(name) {
+        var menu = navMenus[name];
+        if (menu === undefined) {
+            navMenus[name] = menu = new NavMenu();
+        }
+        return menu;
+    }
+    exports.getNavMenu = getNavMenu;
+});
 jsns.define("htmlrest.widgets.pagenumbers", [
     "htmlrest.toggles",
     "htmlrest.eventhandler"
---
+++ widgets/navmenu.js
@@ -0,0 +1,40 @@
+﻿jsns.define("htmlrest.widgets.navmenu", [
+    "htmlrest.eventhandler",
+    "htmlrest.controller"
+],
+function (exports, module, EventHandler, controller) {
+    var navMenus = {};
+
+    function NavMenu() {
+        var menuItems = [];
+
+        var itemAdded = new EventHandler();
+        this.itemAdded = itemAdded.modifier;
+
+        function add(name, controllerConstructor) {
+            if (controllerConstructor !== undefined) {
+                controllerConstructor = controller.createOnCallback(controllerConstructor);
+            }
+            menuItems.push({
+                name: name,
+                created: controllerConstructor
+            });
+            itemAdded.fire(item);
+        }
+        this.add = add;
+
+        function getItems() {
+            return menuItems;
+        }
+        this.getItems = getItems;
+    }
+
+    function getNavMenu(name) {
+        var menu = navMenus[name];
+        if (menu === undefined) {
+            navMenus[name] = menu = new NavMenu();
+        }
+        return menu;
+    }
+    exports.getNavMenu = getNavMenu;
+});
