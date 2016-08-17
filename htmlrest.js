9e5d191c1240080e110018bcbf90b761f59f5480
Andrew Piper <piper.andrew@spcollege.edu>
2016-08-17 07:49:00 -0400

    Added a timed trigger, this makes it easy to delay fire events.

--- htmlrest.js
+++ htmlrest.js
@@ -2057,6 +2057,60 @@ function (exports, module, escape, typeId) {
 });
 "use strict";
 
+jsns.define("htmlrest.timedtrigger", [
+    "htmlrest.eventhandler"
+],
+function (exports, module, EventHandler) {
+    function TimedTrigger(delay) {
+        if (delay === undefined) {
+            delay = 400;
+        }
+        
+        var _delay = delay;
+        var holder;
+        var handler = new EventHandler();
+        var args;
+        
+        this.handler = handler.modifier;
+
+        function setDelay(delay) {
+            _delay = delay;
+        }
+        this.setDelay = setDelay;
+
+        function stop() {
+            clearTimeout(holder);
+            args = undefined;
+        }
+        this.stop = stop;
+
+        function fire() {
+            stop();
+            holder = window.setTimeout(fireHandler, _delay);
+            args = arguments;
+        }
+        this.fire = fire;
+
+        function addListener(context, listener) {
+            handler.modifier.add(context, listener);
+        }
+        this.addListener = addListener;
+
+        function removeListener(context, listener) {
+            handler.modifier.remove(context, listener);
+        }
+        this.removeListener = removeListener;
+
+        function fireHandler() {
+            handler.fire.apply(handler, args);
+        }
+
+    }
+
+    module.exports = TimedTrigger;
+});
+"use strict";
+
 jsns.define("htmlrest.toggles", [
     "htmlrest.typeidentifiers"
 ],
