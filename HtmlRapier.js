a2b0a7417f586fc38213cba34f51f62d810dbd01
Andrew Piper <piper.andrew@spcollege.edu>
2016-08-18 11:58:43 -0400

    Removed iter.

--- HtmlRapier.js
+++ HtmlRapier.js
@@ -1501,47 +1501,6 @@ function(exports, module, toggles, rest){
 });
 "use strict";
 
-/**
- * @callback hr_iter_cb
- */
-
-/**
- * @callback hr_iter
- * @param {array} items - the items to iterate
- * @param {hr_iter_cb} - the function to transform each object
- * @returns the transformed item and null when all items are iterated
- *
- * Iter defines a function that will return a function that iterates
- * over a collection. An iterator here is a single function with no argument
- * that returns null when its work is complete. This matches functions as data
- * used elsewhere. This makes an easy way to transform data before displaying it
- * by calling iter on the collection and then returning what you want from the callback.
- * 
- * You don't new this just call it e.g. iter(things, function(thing){ return thing + ' changes' });
- */
-jsns.define("hr.iter", null,
-function(exports, module){
-
-    /**
-     * Iterate over a collection of items calling cb for each one.
-     * @param {array} items
-     * @param {hr_iter_cb} cb
-     * @returns - The transformed item and null when all items are iterated.
-     */
-    function iter(items, cb) {
-        var i = 0;
-        return function () {
-            if (i < items.length) {
-                return cb(items[i++]);
-            }
-            return null;
-        }
-    }
-
-    module.exports = iter;
-});
-"use strict";
-
 jsns.define("hr.iterable", [
     "hr.typeidentifiers"
 ],
