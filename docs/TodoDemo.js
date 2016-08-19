0c3ba4a6a6858d32ad91b9bdca694dd4b423f76f
Andrew Piper <piper.andrew@spcollege.edu>
2016-08-19 11:12:59 -0400

    Added a controller to handle individual items.

--- docs/TodoDemo.js
+++ docs/TodoDemo.js
@@ -9,6 +9,7 @@ jsns.run([
         var itemsModel = bindings.getModel('items');
         itemsModel.clear();
         var todoItems = [];
+        var self = this;
 
         function findItemVariant(item){
             if (item.important === "on") {
@@ -19,7 +20,7 @@ jsns.run([
         function addItem(evt) {
             evt.preventDefault();
             var item = addModel.getData();
-            itemsModel.appendData(item, null, findItemVariant);
+            itemsModel.appendData(item, controller.createOnCallback(TodoItemController, self), findItemVariant);
             todoItems.push(item);
         }
         this.addItem = addItem;
@@ -34,8 +35,30 @@ jsns.run([
         this.removeItem = removeItem;
 
         function rebuildList() {
-            todoItems.setData(todoItems, null, findItemVariant);
+            itemsModel.setData(todoItems, controller.createOnCallback(TodoItemController, self), findItemVariant);
         }
+        this.rebuildList = rebuildList;
+    }
+
+    function TodoItemController(bindings, context, itemData) {
+        var todoDemoController = context;
+
+        function edit(evt) {
+            evt.preventDefault();
+            //This is a silly way to edit, but trying to keep it short.
+            itemData.text = window.prompt("Edit Todo Item", itemData.text);
+            todoDemoController.rebuildList();
+        }
+        this.edit = edit;
+
+        function deleteItem(evt) {
+            evt.preventDefault();
+            //Silly to use confirm, but done for size
+            if (window.confirm("Do you want to delete " + itemData.text + "?")) {
+                todoDemoController.removeItem(itemData);
+            }
+        }
+        this.deleteItem = deleteItem;
     }
 
     controller.create("todo", TodoDemoController);
