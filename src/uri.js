29a35ef66b67244532d0f108e33c5596f121b666
Andrew Piper <piper.andrew@spcollege.edu>
2016-09-06 14:36:49 -0400

    Added a module to handle uris.

---
+++ src/uri.js
@@ -0,0 +1,65 @@
+﻿"use strict";
+
+jsns.define("hr.uri", ["hr.escape"],
+function (exports, module, escape) {
+    /**
+     * Get an object with the values from the query string. These values
+     * will be sent through the escape function to help prevent xss before
+     * you get the values back. All query string names will be set to lower case
+     * to make looking them back up possible no matter the url case.
+     * @returns {type} 
+     */
+    function getQueryObject() {
+        var qs = window.location.search.substr(1).split('&');
+        var val = {};
+        for (var i = 0; i < qs.length; ++i) {
+            var pair = qs[i].split('=', 2);
+            if (pair.length === 1) {
+                val[pair[0].toLowerCase()] = "";
+            }
+            else if (pair.length > 0) {
+                val[pair[0].toLowerCase()] = escape(decodeURIComponent(pair[1].replace(/\+/g, ' ')));
+            }
+        }
+        return val;
+    }
+
+    exports.getQueryObject = getQueryObject;
+
+    // parseUri 1.2.2
+    // (c) Steven Levithan <stevenlevithan.com>
+    // MIT License
+    // http://blog.stevenlevithan.com/archives/parseuri
+
+    var parseUriOptions = {
+        strictMode: false,
+        key: ["source", "protocol", "authority", "userInfo", "user", "password", "host", "port", "relative", "path", "directory", "file", "query", "anchor"],
+        q: {
+            name: "queryKey",
+            parser: /(?:^|&)([^&=]*)=?([^&]*)/g
+        },
+        parser: {
+            strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
+            loose: /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
+        }
+    };
+
+    function parseUri(str) {
+        var o = parseUriOptions,
+            m = o.parser[o.strictMode ? "strict" : "loose"].exec(str),
+            uri = {},
+            i = 14;
+
+        while (i--) uri[o.key[i]] = m[i] || "";
+
+        uri[o.q.name] = {};
+        uri[o.key[12]].replace(o.q.parser, function ($0, $1, $2) {
+            if ($1) uri[o.q.name][$1] = $2;
+        });
+
+        return uri;
+    };
+
+    exports.parseUri = parseUri;
+});
+
