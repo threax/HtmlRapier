fc42583f338f8e9642f44c6f8725af2ed7725b38
Andrew Piper <piper.andrew@spcollege.edu>
2016-09-26 15:22:13 -0400

    Added open and close events to htmlrapier.

--- plugin/bootstrap.native.js
+++ plugin/bootstrap.native.js
@@ -1,12 +1,27 @@
 ﻿"use strict";
 
 jsns.run([
-    "hr.toggles"
+    "hr.toggles",
+    "hr.eventhandler"
 ],
-function(exports, module, toggles){
+function(exports, module, toggles, EventHandler){
 
     function ModalToggle(element, next) {
+        var onEventHandler = new EventHandler();
+        var offEventHandler = new EventHandler();
+
+        this.onEvent = onEventHandler.modifier;
+        this.offEvent = offEventHandler.modifier;
+
         var modal = new Modal(element);
+
+        element.addEventListener("hidden.bs.modal", function (evt) {
+            offEventHandler.fire();
+        });
+
+        element.addEventListener("shown.bs.modal", function (evt) {
+            onEventHandler.fire();
+        });
 
         function on() {
             modal.open();
