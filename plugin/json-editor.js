064a0658654b1bbcbf459b5db0870e20f1810012
Andrew Piper <piper.andrew@spcollege.edu>
2016-09-27 10:23:28 -0400

    Added json editor and swagger plugins.

---
+++ plugin/json-editor.js
@@ -0,0 +1,48 @@
+﻿jsns.define("json-editor", [
+    
+],
+function (exports, module) {
+    "use strict";
+    if (window.JSONEditor !== undefined) {
+        JSONEditor.defaults.theme = 'bootstrap3';
+        JSONEditor.defaults.disable_collapse = true;
+        JSONEditor.defaults.disable_edit_json = true;
+        JSONEditor.defaults.disable_properties = true;
+        exports.JSONEditor = window.JSONEditor;
+
+        /**
+         * A model interface for the json editor.
+         * @param {JSONEditor} editor - The editor to wrap.
+         */
+        function Model(editor){
+            this.setData = function (data) {
+                editor.root.setValue(data, true);
+            }
+
+            this.appendData = this.setData;
+
+            function clear() {
+                editor.root.setValue(null, true);
+            }
+            this.clear = clear;
+
+            this.getData = function () {
+                return editor.getValue();
+            }
+
+            this.getSrc = function () {
+                return null;
+            }
+
+            this.getEditor = function () {
+                return editor;
+            }
+        }
+        exports.Model = Model;
+
+        function create(element, options) {
+            return new Model(new exports.JSONEditor(element, options));
+        }
+        exports.create = create;
+    }
+});
---
+++ plugin/swagger.js
@@ -0,0 +1,11 @@
+﻿jsns.define("swagger", [
+    "hr.http"
+],
+function (exports, module, http) {
+    "use strict";
+
+    if (window.SwaggerClient !== undefined) {
+        exports.SwaggerClient = window.SwaggerClient;
+        exports.Client = window.SwaggerClient;
+    }
+});
