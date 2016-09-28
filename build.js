4fe5e89bcdbf5809b2dd8e4716ccc76e12c62551
Andrew Piper <piper.andrew@spcollege.edu>
2016-09-28 15:02:31 -0400

    Added build script and gulpfile (calls build script) also added a new dist folder.

---
+++ build.js
@@ -0,0 +1,15 @@
+﻿var ez = require('gulp-build-shortcuts');
+
+module.exports = function (rootDir, outDir) {
+    ez.minifyConcat({
+        libs: [
+            __dirname + "/src/jsns.js",
+            __dirname + "/src/polyfill.js",
+            __dirname + "/src/**/*.js",
+            "!**/*.intellisense.js"
+        ],
+        output: "HtmlRapier",
+        dest: outDir,
+        sourceRoot: __dirname + "/custom_components/HtmlRapier/src/"
+    });
+}
