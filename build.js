e4891c7575ef9c5b3c6ff387086a9999d177f79d
Andrew Piper <threax@users.noreply.github.com>
2016-10-01 15:53:53 -0400

    Started using the gulp toolkit.

--- build.js
+++ build.js
@@ -1,18 +1,4 @@
-﻿var ez = require('gulp-build-shortcuts');
-
-var gulp = require("gulp"),
-    concat = require("gulp-concat"),
-    uglify = require("gulp-uglify"),
-    rename = require("gulp-rename"),
-    sourcemaps = require("gulp-sourcemaps");
-var less = require('gulp-less');
-var uglifycss = require('gulp-uglifycss');
-var gutil = require('gulp-util');
-var plumber = require('gulp-plumber');
-var ts = require('gulp-typescript');
-var es = require('event-stream');
-var path = require('path');
-var jsnsAmdWrapper = require('jsns/amd-wrapper.js');
+﻿var compileJsnsTs = require('threax-gulp-tk/typescript.js');
 
 module.exports = function (rootDir, outDir) {
     return compileJsnsTs({
@@ -31,29 +17,3 @@ module.exports = function (rootDir, outDir) {
         concat: true
     });
 }
-
-function compileJsnsTs(settings) {
-
-    var piped = gulp.src(settings.libs, { base: settings.base })
-        .pipe(sourcemaps.init())
-        .pipe(ts({
-            noImplicitAny: false,
-            allowJs: true,
-            isolatedModules: true,
-            module: 'amd'
-        }))
-        .pipe(jsnsAmdWrapper(settings));
-
-    if (settings.concat === true) {
-        piped = piped.pipe(concat(settings.output + '.js'))
-            .pipe(rename(settings.output + '.min.js'));
-    }
-
-    //.pipe(uglify())
-    piped = piped
-        .pipe(sourcemaps.write(".", { includeContent: false, sourceRoot: settings.sourceRoot }))
-        .pipe(gulp.dest(settings.dest));
-
-    return piped;
-};
-module.exports.prototype.compileJsnsTs = compileJsnsTs;
