ac7e61fb3b96c817d995e1c426c847b11e5f9db0
Andrew Piper <piper.andrew@spcollege.edu>
2016-11-01 14:43:54 -0400

    Added a simple post method also.

--- src/http.ts
+++ src/http.ts
@@ -15,11 +15,35 @@ export function get<T>(url: string) : Promise<T> {
             "Content-Type": "application/json; charset=UTF-8"
         }
     }).then((response) => {
-        return processClientGet(response);
+        return processResult(response);
     });
 }
 
-function processClientGet<T>(response: Response) : Promise<T> {
+/**
+ * A simple function to post to a url. This still uses fetch, but
+ * simplifies its usage. If you need something more advanced use
+ * fetch directly.
+ */
+export function post<T>(url: string, data?:any) : Promise<T> {
+    var body = undefined;
+
+    if(data !== undefined){
+        body = JSON.stringify(data);
+    }
+
+    return fetch(url, {
+        method: "POST",
+        cache: "no-cache",
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8"
+        },
+        body: body
+    }).then((response) => {
+        return processResult(response);
+    });
+}
+
+function processResult<T>(response: Response) : Promise<T> {
     return response.text().then((data) => {
         let resultData = data === "" ? null : JSON.parse(data);
         if (response.status > 199 && response.status < 300) {
