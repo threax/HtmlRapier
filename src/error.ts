194d38dd7cd8b873ad5c0d16f31167b70b314140
Andrew Piper <piper.andrew@spcollege.edu>
2017-01-05 09:45:11 -0500

    Added an error interface.

---
+++ src/error.ts
@@ -0,0 +1,24 @@
+/**
+ * This interface defines a common way to handle validation errors for a model.
+ */
+export interface ValidationError extends Error{
+    /**
+     * Get the validation error named name.
+     */
+    getValidationError(name: string): string | undefined;
+
+    /**
+     * Check to see if a named validation error exists.
+     */
+    hasValidationError(name: string): boolean;
+
+    /**
+     * Get all validation errors.
+     */
+    getValidationErrors() : string[];
+
+    /**
+     * Determine if there are any validation errors.
+     */
+    hasValidationErrors() : boolean;
+}
