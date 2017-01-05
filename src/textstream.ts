6f5e4d007622938b002cb3086bceedc586188d7b
Andrew Piper <piper.andrew@spcollege.edu>
2017-01-05 12:31:56 -0500

    Changed textstream to a typescript class.

--- src/textstream.ts
+++ src/textstream.ts
@@ -59,91 +59,92 @@ function format(data, streamNodes) {
  * @param {type} text
  * @returns {type} 
  */
-export function TextStream(text: string, open?: string, close?: string) {
-    if(open === undefined){
-        open = '{';
-    }
+export class TextStream {
+    private streamNodes = [];
+    private variablesFound = false;
 
-    if(close === undefined){
-        close = '}';
-    }
+    constructor(text: string, open?: string, close?: string){
+        if(open === undefined){
+            open = '{';
+        }
 
+        if(close === undefined){
+            close = '}';
+        }
 
-    var streamNodes = [];
-    var foundVariable = false;
+        var textStart = 0;
+        var bracketStart = 0;
+        var bracketEnd = 0;
+        var bracketCount = 0;
+        var bracketCheck = 0;
+        var leadingText;
+        var variable;
+        var bracketVariable;
+        //This holds text we have not created a TextNode for as we parse, this way we can combine output variables with surrounding text for the stream itself
+        var skippedTextBuffer = "";
+        for (var i = 0; i < text.length; ++i) {
 
-    var textStart = 0;
-    var bracketStart = 0;
-    var bracketEnd = 0;
-    var bracketCount = 0;
-    var bracketCheck = 0;
-    var leadingText;
-    var variable;
-    var bracketVariable;
-    //This holds text we have not created a TextNode for as we parse, this way we can combine output variables with surrounding text for the stream itself
-    var skippedTextBuffer = "";
-    for (var i = 0; i < text.length; ++i) {
-
-        if (text[i] == open) {
-            //Count up opening brackets
-            bracketStart = i;
-            bracketCount = 1;
-            while (++i < text.length && text[i] == open) {
-                ++bracketCount;
-            }
-
-            //Find closing bracket chain, ignore if mismatched or whitespace
-            bracketCheck = bracketCount;
-            while (++i < text.length) {
-                if ((text[i] == close && --bracketCheck == 0) || /\s/.test(text[i])) {
-                    break;
-                }
-            }
-
-            //If the check got back to 0 we found a variable
-            if (bracketCheck == 0) {
-                leadingText = text.substring(textStart, bracketStart);
-
-                bracketEnd = i;
-                bracketVariable = text.substring(bracketStart, bracketEnd + 1);
-
-                switch (bracketCount) {
-                    case 1:
-                        //1 bracket, add to buffer
-                        skippedTextBuffer += leadingText + bracketVariable;
-                        break;
-                    case 2:
-                        streamNodes.push(new TextNode(skippedTextBuffer + leadingText));
-                        skippedTextBuffer = ""; //This is reset every time we actually output something
-                        variable = bracketVariable.substring(2, bracketVariable.length - 2);
-                        if (variable === "this") {
-                            streamNodes.push(new ThisVariableNode());
-                        }
-                        else {
-                            streamNodes.push(new VariableNode(variable));
-                        }
-                        break;
-                    default:
-                        //Multiple brackets, escape by removing one and add to buffer
-                        skippedTextBuffer += leadingText + bracketVariable.substring(1, bracketVariable.length - 1);
-                        break;
+            if (text[i] == open) {
+                //Count up opening brackets
+                bracketStart = i;
+                bracketCount = 1;
+                while (++i < text.length && text[i] == open) {
+                    ++bracketCount;
                 }
 
-                textStart = i + 1;
-                foundVariable = true;
+                //Find closing bracket chain, ignore if mismatched or whitespace
+                bracketCheck = bracketCount;
+                while (++i < text.length) {
+                    if ((text[i] == close && --bracketCheck == 0) || /\s/.test(text[i])) {
+                        break;
+                    }
+                }
+
+                //If the check got back to 0 we found a variable
+                if (bracketCheck == 0) {
+                    leadingText = text.substring(textStart, bracketStart);
+
+                    bracketEnd = i;
+                    bracketVariable = text.substring(bracketStart, bracketEnd + 1);
+
+                    switch (bracketCount) {
+                        case 1:
+                            //1 bracket, add to buffer
+                            skippedTextBuffer += leadingText + bracketVariable;
+                            break;
+                        case 2:
+                            this.streamNodes.push(new TextNode(skippedTextBuffer + leadingText));
+                            skippedTextBuffer = ""; //This is reset every time we actually output something
+                            variable = bracketVariable.substring(2, bracketVariable.length - 2);
+                            if (variable === "this") {
+                                this.streamNodes.push(new ThisVariableNode());
+                            }
+                            else {
+                                this.streamNodes.push(new VariableNode(variable));
+                            }
+                            break;
+                        default:
+                            //Multiple brackets, escape by removing one and add to buffer
+                            skippedTextBuffer += leadingText + bracketVariable.substring(1, bracketVariable.length - 1);
+                            break;
+                    }
+
+                    textStart = i + 1;
+                    this.variablesFound = true;
+                }
             }
+        }
+
+        if (textStart < text.length) {
+            this.streamNodes.push(new TextNode(skippedTextBuffer + text.substring(textStart, text.length)));
         }
     }
 
-    if (textStart < text.length) {
-        streamNodes.push(new TextNode(skippedTextBuffer + text.substring(textStart, text.length)));
+    public format(data) {
+        return format(data, this.streamNodes);
     }
 
-    this.format = function (data) {
-        return format(data, streamNodes);
-    }
-
-    this.foundVariable = function () {
-        return foundVariable;
+    public foundVariable() {
+        return this.variablesFound;
     }
 }
