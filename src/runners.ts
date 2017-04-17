6e2b6e44a7d37032fafe6394f4ad4e8d9eb8c5b6
Andrew Piper <piper.andrew@spcollege.edu>
2017-04-17 11:28:37 -0400

    Added a way to run modules from a html element

--- src/runners.ts
+++ src/runners.ts
@@ -1,1 +1,13 @@
-(<any>window).jsns.runNamedAmd("hr.componentgatherer");
+(function () {
+    var jsns = (<any>window).jsns;
+    jsns.runNamedAmd("hr.componentgatherer");
+
+    var runnerElements = document.querySelectorAll('[data-hr-run]');
+    for (var i = 0; i < runnerElements.length; ++i) {
+        var runnerElement = runnerElements[i];
+        var runnerAttr = runnerElement.getAttribute('data-hr-run');
+        if (runnerAttr) {
+            jsns.runNamedAmd(runnerAttr);
+        }
+    }
+})();
