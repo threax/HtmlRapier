21d04ad2fb21b2e4e0f09133b28892e4600087b1
Andrew Piper <piper.andrew@spcollege.edu>
2017-04-24 10:30:15 -0400

    Moved the attribute run function to a module.

---
+++ src/runattributes.ts
@@ -0,0 +1,18 @@
+///<amd-module name="hr.inlinerunners"/>
+
+"use strict";
+
+declare var jsns;
+
+//Find all data-hr-run attributes and run the runner they specify, it does not matter what kind of element
+//contains the runner.
+var runnerElements = document.querySelectorAll('[data-hr-run]');
+for (var i = 0; i < runnerElements.length; ++i) {
+    var runnerElement = runnerElements[i];
+    var runnerAttr = runnerElement.getAttribute('data-hr-run');
+    if (runnerAttr) {
+        jsns.run(runnerAttr);
+    }
+}
+
+export var ran = true; //Dummy operation to force this to be a module
--- src/runners.ts
+++ src/runners.ts
@@ -1,14 +1,4 @@
 declare var jsns;
 
-(function () {
-    jsns.run("hr.componentgatherer");
-
-    var runnerElements = document.querySelectorAll('[data-hr-run]');
-    for (var i = 0; i < runnerElements.length; ++i) {
-        var runnerElement = runnerElements[i];
-        var runnerAttr = runnerElement.getAttribute('data-hr-run');
-        if (runnerAttr) {
-            jsns.run(runnerAttr);
-        }
-    }
-})();
+jsns.run("hr.componentgatherer");
+jsns.run("hr.runattributes");
