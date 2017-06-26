588720fe9fc569a81c8f9e1ca12b59a5ab8d4fb8
AndrewPiper <AndrewPiper@nowhere.com>
2017-06-26 15:14:33 -0400

    Finished updating simple form input.

--- docs/demos/simple-form-input.ts
+++ docs/demos/simple-form-input.ts
@@ -3,6 +3,7 @@
 "use strict";
 import * as controller from "hr.controller";
 
+//Create an interface for the data we are handling, so we know what we can put on the form and in the view.
 interface PersonInfo{
     userName: string;
 }
@@ -16,10 +17,15 @@ class FormDemoController{
     private output: controller.IView<PersonInfo>;
 
     public constructor(bindings: controller.BindingCollection){
+        //Get the form for the input
         this.input = bindings.getForm<PersonInfo>("input");
+
+        //Get the view
         this.output = bindings.getView<PersonInfo>("output");
     }
 
+    //This function will be bound to the event labeled data-hr-event-submit="outputInfo"
+    //It should be written as if you were adding it to addEventListener.
     public outputInfo(evt: Event): void {
         evt.preventDefault();
         var data = this.input.getData();
