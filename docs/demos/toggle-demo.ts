cbff773fe2994ebc137e6a59861caef7dcd59061
AndrewPiper <AndrewPiper@nowhere.com>
2017-06-26 15:24:22 -0400

    Updated toggle demo.

---
+++ docs/demos/toggle-demo.ts
@@ -0,0 +1,31 @@
+﻿///<amd-module name="toggle-demo"/>
+
+"use strict";
+import * as controller from "hr.controller";
+
+class ToggleDemoController{
+    public static get InjectorArgs(): controller.DiFunction<any>[] {
+        return [controller.BindingCollection];
+    }
+
+    private toggle: controller.OnOffToggle;
+    private on: boolean = false;
+
+    public constructor(bindings: controller.BindingCollection){
+        this.toggle = bindings.getToggle('color');
+    }
+
+    public toggleColors(evt: Event): void {
+        this.on = !this.on;
+        if (this.on) {
+            this.toggle.on();
+        }
+        else {
+            this.toggle.off();                
+        }
+    }
+}
+
+var builder = new controller.InjectedControllerBuilder();
+builder.Services.addTransient(ToggleDemoController, ToggleDemoController);
+builder.create("toggleDemo", ToggleDemoController);
