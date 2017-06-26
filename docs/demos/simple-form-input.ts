1ce5252af18452d98e08825475264671380b6028
AndrewPiper <AndrewPiper@nowhere.com>
2017-06-26 15:04:27 -0400

    Moved remaining demos

---
+++ docs/demos/simple-form-input.ts
@@ -0,0 +1,32 @@
+﻿///<amd-module name="simple-form-input-demo"/>
+
+"use strict";
+import * as controller from "hr.controller";
+
+interface PersonInfo{
+    userName: string;
+}
+
+class FormDemoController{
+    public static get InjectorArgs(): controller.DiFunction<any>[] {
+        return [controller.BindingCollection];
+    }
+
+    private input: controller.IForm<PersonInfo>;
+    private output: controller.IView<PersonInfo>;
+
+    public constructor(bindings: controller.BindingCollection){
+        this.input = bindings.getForm<PersonInfo>("input");
+        this.output = bindings.getView<PersonInfo>("output");
+    }
+
+    public outputInfo(evt: Event): void {
+        evt.preventDefault();
+        var data = this.input.getData();
+        this.output.setData(data);
+    }
+}
+
+var builder = new controller.InjectedControllerBuilder();
+builder.Services.addTransient(FormDemoController, FormDemoController);
+builder.create("inputDemo", FormDemoController);
