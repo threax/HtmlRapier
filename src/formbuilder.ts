d3a6e8efa59656098c087a18cf3a4950f338d6b9
Andrew Piper <piper.andrew@spcollege.edu>
2017-07-21 10:02:31 -0400

    Trying to add a class to do forms.

---
+++ src/formbuilder.ts
@@ -0,0 +1,55 @@
+import * as component from 'hr.components';
+
+interface JsonSchema {
+    title?: string;
+    type?: string;
+    additionalProperties?: boolean;
+    properties?: JsonPropertyMap;
+}
+
+interface JsonProperty {
+    title?: string;
+    type?: string | string[];
+    format?: string;
+    "x-ui-order"?: number;
+    "x-values"?: JsonLabel[];
+}
+
+interface ProcessedJsonProperty extends JsonProperty {
+    buildName: string;
+}
+
+type JsonPropertyMap = { [key: string]: JsonProperty };
+
+interface JsonLabel {
+    label: string;
+    value: any;
+}
+
+export function buildForm(componentName: string, schema: JsonSchema, formElement: HTMLFormElement) {
+    ////Clear existing elements
+    //while (formElement.lastChild) {
+    //    formElement.removeChild(formElement.lastChild);
+    //}
+    
+    var propArray: ProcessedJsonProperty[] = [];
+    var props = schema.properties;
+    if (props) {
+        for(var key in props){
+            var processed = Object.create(props[key]);
+            processed.buildName = key;
+            propArray.push(processed);
+        }
+
+        propArray.sort((a, b) =>{
+            return a["x-ui-order"] - b["x-ui-order"];
+        });
+
+        for(var i = 0; i < propArray.length; ++i){
+            var variantFinder = (i) =>{
+                return null;
+            };
+            component.one(componentName, propArray[i], formElement, undefined, undefined, undefined);
+        }
+    }
+}
