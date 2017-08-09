0c492c52cbef0ff02fe2a3b44a2648ffd2fb776a
Andrew Piper <piper.andrew@spcollege.edu>
2017-08-09 11:25:09 -0400

    Got history pop state working.

--- src/history.ts
+++ src/history.ts
@@ -1,10 +1,23 @@
 import { Uri } from 'hr.uri';
 import * as di from 'hr.di';
 
+export interface IHistoryHandler<T> {
+    onPopState(data: T);
+}
+
 export abstract class IHistoryManager {
-    public abstract pushQueryState(query: any): void;
+    public abstract registerHandler<T>(name: string, handler: IHistoryHandler<T>);
+
+    public abstract pushQueryState<T extends {}>(handler: string, query: any): void;
+
+    public abstract replaceQueryState<T extends {}>(handler: string, query: any): void;
 
     public abstract getCurrentQuery(): {};
+}
+
+interface IHistoryEntry<T> {
+    handler: string;
+    data: T;
 }
 
 export class HistoryManager {
@@ -13,16 +26,52 @@ export class HistoryManager {
     }
 
     private pageBaseUrl: string;
+    private handlers: { [key: string]: IHistoryHandler<any> } = {};
 
     constructor(pageBaseUrl: string) {
         this.pageBaseUrl = pageBaseUrl;
+        window.addEventListener("popstate", (evt: PopStateEvent) => this.handlePopState(evt));
     }
 
-    public pushQueryState(query: any): void {
+    private handlePopState(evt: PopStateEvent) {
+        evt.preventDefault();
+        evt.stopPropagation();
+        var state: IHistoryEntry<any> = evt.state;
+        if (state) {
+            var handler = this.handlers[state.handler];
+            if (handler !== undefined) {
+                handler.onPopState(state.data);
+            }
+        }
+    }
+
+    public registerHandler<T>(name: string, handler: IHistoryHandler<T>) {
+        if(this.handlers[name] !== undefined){
+            throw new Error("Attempted to register an IHistoryHandler named '" + name + "' multiple times, only one is allowed.");
+        }
+        this.handlers[name] = handler;
+    }
+
+    public pushQueryState<T extends {}>(handler: string, query: T): void {
         var uri = new Uri();
         uri.path = this.pageBaseUrl;
         uri.setQueryFromObject(query);
-        history.pushState(query, document.title, uri.build());
+        var historyObj: IHistoryEntry<T> = {
+            handler: handler,
+            data: query
+        };
+        history.pushState(historyObj, document.title, uri.build());
+    }
+
+    public replaceQueryState<T extends {}>(handler: string, query: T): void {
+        var uri = new Uri();
+        uri.path = this.pageBaseUrl;
+        uri.setQueryFromObject(query);
+        var historyObj: IHistoryEntry<T> = {
+            handler: handler,
+            data: query
+        };
+        history.replaceState(historyObj, document.title, uri.build());
     }
 
     public getCurrentQuery(): {} {
