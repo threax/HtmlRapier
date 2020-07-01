aca02987067cf6911f7316d656a86877331b3aa6
Andrew Piper <andrew@threax.com>
2020-07-01 11:00:15 -0400

    Renamed hr.ts to main.ts.

---
+++ main.ts
@@ -0,0 +1,9 @@
+import * as formbuilder from 'htmlrapier/src/formbuilder';
+import * as componentgatherer from 'htmlrapier/src/componentgatherer';
+
+formbuilder.setup();
+componentgatherer.setup();
+
+export function setup() {
+    return true;
+}
