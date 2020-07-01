0fb773eddc96977bbb169c2503bd3654fb62809c
Andrew Piper <andrew@threax.com>
2020-07-01 09:41:26 -0400

    Added a hr base import file. This actually eliminates the need for the runners.

---
+++ src/hr.ts
@@ -0,0 +1,9 @@
+export { BindingCollection } from './bindingcollection';
+export { OnOffToggle, TypedToggle } from './toggles';
+export { DiFunction, ServiceCollection, InjectableArgs } from './di';
+export { IForm } from './form';
+export { IView } from './view';
+export { InjectedControllerBuilder, InjectControllerData } from './controller';
+
+import * as formbuilder from './formbuilder';
+import * as componentgatherer from './componentgatherer'
