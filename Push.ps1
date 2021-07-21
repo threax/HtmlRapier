a6d1d51e19369fe52ead782486123e10573daa6c
Andrew Piper <andrew@threax.com>
2021-07-21 12:02:30 -0400

    Add push file.

---
+++ Push.ps1
@@ -0,0 +1,4 @@
+git push
+git push https://github.com/threax/HtmlRapier.git
+git push https://gitlab.com/threax-software/htmlrapier.git
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/HtmlRapier
