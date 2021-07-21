d224ca6391bac1a46391e3811b30972608b89528
Andrew Piper <andrew@threax.com>
2021-07-21 13:29:42 -0400

    Add tag push.

--- Push.ps1
+++ Push.ps1
@@ -1,4 +1,8 @@
 git push
+git push --tags
 git push https://github.com/threax/HtmlRapier.git
+git push https://github.com/threax/HtmlRapier.git --tags
 git push https://gitlab.com/threax-software/htmlrapier.git
+git push https://gitlab.com/threax-software/htmlrapier.git --tags
 git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/HtmlRapier
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/HtmlRapier --tags
