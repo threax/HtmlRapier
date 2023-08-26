126d14d626a89411d4583bf936454e63c969e8fd
Andrew Piper <andrew@threax.com>
2023-08-26 10:30:00 -0400

    Modify event dispatchers to make sure all events fire and they print exceptions.

--- src/eventdispatcher.ts
+++ src/eventdispatcher.ts
@@ -14,7 +14,11 @@ export interface EventModifier<T>{
 }
 
 /**
- * This event dispatcher does not handle event listeners returning values.
+ * Track listeners and fire events. If an event handler throws an error the error
+ * will be printed to the console, but all other listeners will still fire and the caller
+ * will not know about the error. This is intended to make consuming events this handler
+ * fires work like an event coming off an html element where the exception will be logged,
+ * but the page itself does not break.
  */
 export class ActionEventDispatcher<T>{
     protected listeners: ActionEventListener<T>[] = <any>[];
@@ -40,56 +44,23 @@ export class ActionEventDispatcher<T>{
 
     fire(arg:T){
         for (var i = 0; i < this.listeners.length; ++i) {
-            this.listeners[i](arg);
-        }
-    }
-}
-
-/**
- * This is class is for events that return a value.
- */
-export class FuncEventDispatcher<TRet, TArg>{
-    protected listeners: FuncEventListener<TRet, TArg>[] = <any>[];
-
-    add(listener: FuncEventListener<TRet, TArg>) {
-        if(!typeId.isFunction(listener)){
-            throw new Error("Listener must be a function, instead got " + typeof(listener));
-        }
-        this.listeners.push(listener);
-    }
-
-    remove(listener: FuncEventListener<TRet, TArg>) {
-        for (var i = 0; i < this.listeners.length; ++i) {
-            if (this.listeners[i] === listener) {
-                this.listeners.splice(i--, 1);
+            try {
+                this.listeners[i](arg);
+            }
+            catch (e) {
+                console.error("An error occured firing an ActionEventDispatcher.");
+                console.error(e);
             }
         }
-    }
-
-    get modifier(): EventModifier<FuncEventListener<TRet, TArg>>{
-        return this;
-    }
-
-    fire(arg:TArg){
-        var result : TRet[] = undefined;
-        var nextResult : TRet;
-        for (var i = 0; i < this.listeners.length; ++i) {
-            var listener = this.listeners[i];
-            nextResult = listener(arg);
-            if (nextResult !== undefined) {
-                if (result === undefined) {
-                    result = [];
-                }
-                result.push(nextResult);
-            }
-        }
-        return result;
     }
 }
 
 /**
  * This event dispatcher will return a promise that will resolve when all events
- * are finished running. Allows async work to stay in the event flow.
+ * are finished running. Allows async work to stay in the event flow. This will handle
+ * errors in the same way as the ActionEventDispatcher where they are printed, but not
+ * bubbled up to the caller. Any results that do go through will be returned even if others
+ * return errors.
  */
 export class PromiseEventDispatcher<TRet, TArg>{
     protected listeners: FuncEventListener<Promise<TRet>, TArg>[] = <any>[];
@@ -113,12 +84,7 @@ export class PromiseEventDispatcher<TRet, TArg>{
         return this;
     }
     
-    /**
-     * Fire the event. The listeners can return values, if they do the values will be added
-     * to an array that is returned by the promise returned by this function.
-     * @returns {Promise} a promise that will resolve when all fired events resolve.
-     */
-    fire(arg:TArg): Promise<TRet[]> {
+    async fire(arg:TArg): Promise<TRet[]> {
         var result: TRet[];
         var promises:Promise<void>[] = [];
         for (var i = 0; i < this.listeners.length; ++i) {
@@ -136,9 +102,16 @@ export class PromiseEventDispatcher<TRet, TArg>{
             }));
         }
 
-        return Promise.all(promises)
-            .then(function (data) {
-                return result;
-            });
+        for (var i = 0; i < promises.length; ++i) {
+            try {
+                await promises[i];
+            }
+            catch (e) {
+                console.error("An error occured firing a PromiseEventDispatcher.");
+                console.error(e);
+            }
+        }
+
+        return result;
     }
 }
